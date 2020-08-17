281a55030dce7ee720e1ec5958145f430883dfe9
DanielSoaresRocha <legoliga123@gmail.com>
2020-08-17 14:46:13 -0300

    implementando serviço de dados do dashboard

--- src/app/dashboard/dados.service.ts
+++ src/app/dashboard/dados.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/observable'
+import { observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DadosService {
+  readonly dados = [
+    ['Janeiro', 33],
+    ['Fevereirio', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ]
 
   constructor() { }
+
+  obterDados (): Observable<any> {
+    return new Observable(observable => {
+      observable.next(this.dados);
+      observable.complete();
+    })
+  }
 }
